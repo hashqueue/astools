b23d65de5f3acd92b72609de5a41fe38d32decf2
hashqueue <1912315910@qq.com>
2023-03-28 00:16:14 +0800

    perf: bump goph version to 1.3.1

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ else
   echo "Error: please install Go first."
   exit 4
 fi
-GOOS=android GOARCH=arm64 go build -o astools_android_arm64 cmd/cli/main.go
-GOOS=darwin GOARCH=arm64 go build -o astools_darwin_arm64 cmd/cli/main.go
-GOOS=darwin GOARCH=amd64 go build -o astools_darwin_amd64 cmd/cli/main.go
-GOOS=linux GOARCH=amd64 go build -o astools_linux_amd64 cmd/cli/main.go
-GOOS=linux GOARCH=arm64 go build -o astools_linux_arm64 cmd/cli/main.go
-GOOS=windows GOARCH=arm64 go build -o astools_windows_arm64.exe cmd/cli/main.go
-GOOS=windows GOARCH=amd64 go build -o astools_windows_amd64.exe cmd/cli/main.go
+GOOS=android GOARCH=arm64 CGO_ENABLED=0 go build -o astools_android_arm64 cmd/cli/main.go
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o astools_darwin_arm64 cmd/cli/main.go
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o astools_darwin_amd64 cmd/cli/main.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o astools_linux_amd64 cmd/cli/main.go
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o astools_linux_arm64 cmd/cli/main.go
+GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -o astools_windows_arm64.exe cmd/cli/main.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o astools_windows_amd64.exe cmd/cli/main.go
 echo "build done."
 ls -larth ./astools*
--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.20
 
 require (
 	github.com/bramvdbogaerde/go-scp v1.2.1
-	github.com/melbahja/goph v1.3.0
-	golang.org/x/crypto v0.5.0
+	github.com/melbahja/goph v1.3.1
+	golang.org/x/crypto v0.7.0
 )
 
 require (
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/sftp v1.13.5 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
